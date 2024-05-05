270e702621a1b2f0f4ee00b3b37ed2093fde046a
Arcangelo Chinè <arcangelo.chine@gmail.com>
2024-05-05 12:48:53 +0200

    initial commit

--- client/HOTELIERCustomerClientMain.java
+++ client/HOTELIERCustomerClientMain.java
@@ -1,4 +1,4 @@
-public class HOTELIERCustomerClientMain {
+class HOTELIERCustomerClientMain {
     public static void main(String[] args) {
         System.out.println("Hello, client!");
     }
--- server/HOTELIERServerMain.java
+++ server/HOTELIERServerMain.java
@@ -1,4 +1,4 @@
-public class HOTELIERServerMain {
+class HOTELIERServerMain {
     public static void main(String[] args) {
         System.out.println("Hello, server!");
     }
