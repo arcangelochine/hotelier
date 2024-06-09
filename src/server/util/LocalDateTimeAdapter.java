dd229a48fe312e6e556ce44617549dd82423b7c3
Arcangelo Chinè <arcangelo.chine@gmail.com>
2024-06-09 18:40:21 +0200

    fix date

--- src/server/util/LocalDateTimeAdapter.java
+++ src/server/util/LocalDateTimeAdapter.java
@@ -13,28 +13,11 @@ public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
-        jsonWriter.beginObject();
-        jsonWriter.name("date");
         jsonWriter.value(localDateTime.format(FORMATTER));
-        jsonWriter.endObject();
     }
 
     @Override
     public LocalDateTime read(JsonReader jsonReader) throws IOException {
-        jsonReader.beginObject();
-        LocalDateTime localDateTime = null;
-
-        while (jsonReader.hasNext()) {
-            String name = jsonReader.nextName();
-            if (name.equals("date")) {
-                String dateStr = jsonReader.nextString();
-                localDateTime = LocalDateTime.parse(dateStr, FORMATTER);
-            } else {
-                jsonReader.skipValue();
-            }
-        }
-
-        jsonReader.endObject();
-        return localDateTime;
+        return LocalDateTime.parse(jsonReader.nextString(), FORMATTER);
     }
 }
